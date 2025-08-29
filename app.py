4ef733feb8df4ce2624eb336d5ded7887bcb2c6b
/dev/nullsec <nullsec0x@proton.me>
2025-08-29 13:09:51 +0100

    Update app.py

--- app.py
+++ app.py
@@ -44,8 +44,8 @@ def create_app(config_name=None):
         db.create_all()
 
     return app
+app = create_app()
 
 if __name__ == '__main__':
-    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=5001)
 
